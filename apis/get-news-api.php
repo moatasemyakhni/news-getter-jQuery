1989690650baae48b6a8a0a94821aea0fd1d95ad
= <motasem.yakhni@gmail.com>
2022-09-23 15:50:14 +0300

    display the result

--- apis/get-news-api.php
+++ apis/get-news-api.php
@@ -9,6 +9,15 @@
 
     $results = $query->get_result();
     while($result = $results->fetch_assoc()) {
-        echo '<pre>';
-        print_r($result);
+        echo '
+        <div class="d-flex flex-column shadow mb-5">
+            <div class="d-flex flex-column p-2 bg-dark align-items-center text-white">
+                <h2 class="font-monospace border-bottom border-3 mb-1 rounded">'. $result['title'] .'</h2>
+                <p class="mb-3 text-white-50 align-self-start px-3">'. $result['published'] .'</p>
+                <p class="px-3 align-self-start">
+                '. $result['content'] .'
+                </p>
+            </div>
+        </div>
+        ';
     }

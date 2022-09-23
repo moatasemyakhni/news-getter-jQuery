6406c046adc09f348debe2c71791d7155aa8acf6
= <motasem.yakhni@gmail.com>
2022-09-23 15:42:46 +0300

    link info

--- apis/get-news-api.php
+++ apis/get-news-api.php
@@ -1,2 +1,14 @@
 <?php
-    
+
+    include('connection.php');
+
+    $query = $mysqli->prepare("SELECT * FROM news");
+    if(!$query->execute()) {
+        die("ERROR in SQL statement");
+    }
+
+    $results = $query->get_result();
+    while($result = $results->fetch_assoc()) {
+        echo '<pre>';
+        print_r($result);
+    }
